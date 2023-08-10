5008f0a6fae9a22ac4b26826b6e54bd8667f53e4
mellymer-appdeveloper <141914681+mellymer-appdeveloper@users.noreply.github.com>
2023-08-10 15:13:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blackberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
